a5756b5f1bcbb124d947150d40876c7abcabc553
qq642672661 <642672661@qq.com>
2019-06-16 22:20:53 +0800

    任务7: 请求里的查询参数

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Req, Query } from '@nestjs/common';
+
+@Controller('posts')
+export class PostsController {
+    @Get()
+    index(@Query() query) {
+       console.log(query) 
+        
+        return [
+            {
+                title: 'hello ~'
+            }
+        ];
+    }
+}
